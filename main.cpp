dde7f6b91139a7f1d4036d3e08c016ea27f5c005
ami-sc <github@slmail.ami.sc>
2022-09-13 15:01:13 -0500

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include <QWidget>
+
+int main (int argc, char *argv[])
+{
+    QApplication Ensemble(argc, argv);
+
+    /*** Application Code ***/
+
+    return Ensemble.exec();
+}
